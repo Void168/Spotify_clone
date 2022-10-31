2e40d0e297c1ae183c2d39d95e7522577fe56730
LPHung <phuhung16820@gmail.com>
2022-11-01 00:47:11 +0700

    Top Artists

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,26 @@
-const TopArtists = () => <div>TopArtists</div>;
+import React from 'react'
+import { ArtistCard, Loader, SongCard } from '../components'
 
-export default TopArtists;
+import { useGetTopChartsQuery } from '../redux/services/shazamCore'
+
+const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery()
+
+  if (isFetching) return <Loader title="Đang tải xu hướng" />
+
+  if (error) return <Error />
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Nghệ sĩ nổi bật
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TopArtists
